8121dc5ce23672263cee5c0fadcffa3ab4ec6e94
wangjq4214 <w1637894214@163.com>
2022-12-05 16:31:13 +0800

    fix: 修复token发送与退出问题

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import qs from 'qs';
 import { Message } from '@arco-design/web-vue';
+import { useUserModel } from '@/model';
 
 interface ResponseWrap<T> {
   code: number;
@@ -15,8 +16,20 @@ const instance = axios.create({
   },
 });
 
+instance.interceptors.request.use(req => {
+  const { token } = useUserModel();
+  req.headers = { ...req.headers, Authorization: `ASI ${token.value}` || 'ASI ' };
+
+  return req;
+});
+
 instance.interceptors.response.use(
   (resp: AxiosResponse<ResponseWrap<unknown>>) => {
+    if (resp.status >= 300 || resp.status < 200) {
+      Message.error(resp.statusText);
+      return Promise.reject(new Error(resp.statusText));
+    }
+
     if (resp.data.code !== 0) {
       Message.error(resp.data.msg);
       return Promise.reject(new Error(resp.data.msg));
